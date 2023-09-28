134271fbdea66b706bb3510c82edd24497561163
Maheshwar <mahesh27072001@gmail.com>
2023-09-28 17:47:33 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_X2.mk
+    $(LOCAL_DIR)/blaze_X2.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_X2-userdebug \
-    lineage_X2-eng
+    blaze_X2-userdebug \
+    blaze_X2-eng
